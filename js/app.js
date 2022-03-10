6b9def3d7555291a94817463ccdc015e1584a641
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-10 00:32:24 -0700

    api js refactored

--- js/app.js
+++ js/app.js
@@ -1,3 +1,54 @@
-const ipapi = new Ipapi()
+const ipInput = document.querySelector('#ip-address'),
+      submitBtn = document.querySelector('.submit-btn'),
+      ipNum = document.querySelector('.ip-num'),
+      ipLocation = document.querySelector('.ip-num'),
+      ipTimezone = document.querySelector('.ip-num'),
+      ipIsp = document.querySelector('.ip-num');
+
+
+
+const ipApi = new IpApi()
 
 // ipapi.getIpInfo('197.211.59.104').then(data => console.log(data))
+
+
+var map = L.map('map', {
+  'center': [0,0],
+  'zoom': 0,
+  'layers': [
+    L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&cppy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    })
+  ]
+})
+
+
+updateMarker = (update_marker = [-43, 42]) => {
+  map.setView(update_marker, 13)
+  L.marker(update_marker).addTo(map)
+}
+
+
+getIpDetails = (ipValue) => {
+
+  ipApi.getIpInfo(ipValue).then(data => {
+    console.log(data);
+    ipNum.innerHTML = data.ip
+    ipLocation.innerHTML = `${data.location.city} ${data.location.country} ${data.location.postalCode}`
+    ipTimezone.innerHTML = data.location.timezone
+    ipIsp.innerHTML = data.isp
+
+    updateMarker([data.location.lat, data.location.lng])
+  })
+  .catch(error => alert('Oops! Something went wrong..'))
+}
+
+
+submitBtn.addEventListener('click', (e) => {
+  if (ipInput.value != '' && ipInput.value != null) {
+    // getIpDetails(ipInput.value)
+    getIpDetails(ipInput.value);
+  }
+  alert('Enter a valid IP address')
+})
+
--- js/ipapi.js
+++ js/ipapi.js
@@ -1,14 +1,44 @@
-class Ipapi {
+class IpApi {
   constructor() {
     this.apikey = 'at_qoAx8gLi3Pbp9uHmHgiiKJSYU6mTm';
     this.currentVersion = 'v2'
   }
 
-  async getIpInfo(ipadd) {
-    const addressInfo = await fetch(`https://geo.ipify.org/api/${this.currentVersion}/country?apiKey=${this.apikey}&ipAddress=${ipadd}`);
+  async getIpInfo(ipDetail) {
+
+    let ipUrl;
+  if (ipDetail == undefined) {
+    ipUrl = `https://geo.ipify.org/api/${this.currentVersion}/country,city?apiKey=${this.apikey}`
+  } else {
+    ipUrl = `https://geo.ipify.org/api/${this.currentVersion}/country,city?apiKey=${this.apikey}&ipAddress=${ipDetail}`
+  }
+
+    const addressInfo = await fetch(ipUrl);
 
     const addressData = await addressInfo.json();
 
     return addressData;
   }
+
+
+
+//   getIpDetails(ipDetail) {
+//   let ipUrl;
+//   if (ipDetail == undefined) {
+//     ipUrl = `https://geo.ipify.org/api/${this.currentVersion}/country,city?apiKey=${this.apikey}`
+//   } else {
+//     ipUrl = `https://geo.ipify.org/api/${this.currentVersion}/country,city?apiKey=${this.apikey}&ipAddress=${ipDetail}`
+//   }
+
+//   ipApi.getIpInfo(ipUrl).then(data => {
+//     console.log(data);
+//     ipNum.innerHTML = data.ip
+//     ipLocation.innerHTML = `${data.location.city} ${data.location.country} ${data.location.postalCode}`
+//     ipTimezone.innerHTML = data.location.timezone
+//     ipIsp.innerHTML = data.isp
+
+//     updateMarker([data.location.lat, data.location.lng])
+//   })
+//   .catch(error => alert('Oops! Something went wrong..'))
+// }
 }
